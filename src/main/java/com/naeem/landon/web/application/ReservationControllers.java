f5b7d42d736887c9c22bc550083caea3f5439473
naeemkhan12 <naeemb7070@gmail.com>
2017-04-24 00:28:23 +0500

    populate table with data.

--- src/main/java/com/naeem/landon/web/application/ReservationControllers.java
+++ src/main/java/com/naeem/landon/web/application/ReservationControllers.java
@@ -1,8 +1,20 @@
 package com.naeem.landon.web.application;
-
+import com.fasterxml.jackson.databind.util.StdDateFormat;
+import com.naeem.landon.business.domain.RoomReservation;
+import com.naeem.landon.business.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by lenovo on 4/23/2017.
@@ -10,8 +22,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/reservations")
 public class ReservationControllers {
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+    @Autowired
+    private ReservationService reservationService;
     @RequestMapping(method = RequestMethod.GET)
-    public String getReservations(){
+    public String getReservations(@RequestParam(value = "date" , required = false)String dateString, Model model) {
+        Date date = null;
+        if(dateString!=null) {
+            try {
+                date = DATE_FORMAT.parse(dateString);
+
+            } catch (ParseException ex) {
+                date = new Date();
+            }
+        }else{
+                date = new Date();
+        }
+        List<RoomReservation> roomReservations = this.reservationService.getRoomReservationForDate(date);
+        model.addAttribute("roomReservations" , roomReservations);
         return "reservations";
     }
 
